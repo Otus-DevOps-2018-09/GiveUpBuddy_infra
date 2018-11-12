a99224952676a216cae3576e52a67ca7186e560d
Andrey Nikolaev <giveupbuddy@gmail.com>
2018-11-13 00:01:48 +0700

    Bug with assertEqual fixed

--- play-travis/test.py
+++ play-travis/test.py
@@ -3,7 +3,7 @@ import unittest
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+        self.assertEqual(10-9, 1)
 
 if __name__ == '__main__':
     unittest.main()
